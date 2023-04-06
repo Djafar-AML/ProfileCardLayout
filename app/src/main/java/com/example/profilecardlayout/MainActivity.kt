78fcfbfdd6ba894bfc8df3b7c7f5309ef4b4db74
Jeff <Jafaragha77@gmail.com>
2023-04-06 21:00:32 +0330

    - working on ProfileContent and ProfilePicture

--- app/src/main/java/com/example/profilecardlayout/MainActivity.kt
+++ app/src/main/java/com/example/profilecardlayout/MainActivity.kt
@@ -3,15 +3,19 @@ package com.example.profilecardlayout
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Card
 import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
@@ -40,29 +44,57 @@ fun MainScreen() {
 fun ProfileCard() {
 
     Card(
-        modifier = Modifier.fillMaxWidth(),
-        elevation = 8.dp
+        modifier = Modifier
+            .padding(16.dp)
+            .fillMaxWidth()
+            .wrapContentHeight(align = Alignment.Top),
+        elevation = 8.dp,
     ) {
 
-        Row(modifier = Modifier.wrapContentSize()) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Start
+        ) {
             ProfilePicture()
             ProfileContent()
         }
+
     }
+
 }
 
 @Composable
 fun ProfilePicture() {
 
+    Card(
+        shape = CircleShape,
+        border = BorderStroke(
+            width = 2.dp,
+            color = Color.Green
+        ),
+        modifier = Modifier.padding(16.dp),
+        elevation = 4.dp,
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.profile_picture),
+            contentDescription = "Profile Picture",
+            modifier = Modifier.size(72.dp),
+            contentScale = ContentScale.Crop
+        )
+    }
+
 }
 
 @Composable
 fun ProfileContent() {
-
+    Text(text = "Jeff alpha")
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
+
     MainScreen()
+
 }
